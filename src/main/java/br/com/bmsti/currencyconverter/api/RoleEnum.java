75571467ff5da68732bb8bf32a7683e3dea903ef
Angelo Brandão <angelobms@gmail.com>
2020-08-28 12:49:46 -0300

    Added RoleEnum enum class.

---
+++ src/main/java/br/com/bmsti/currencyconverter/api/RoleEnum.java
@@ -0,0 +1,12 @@
+package br.com.bmsti.currencyconverter.api;
+
+/**
+ * Enum class that represents the existing profiles for a system user.
+ *
+ * @author angelo santos
+ * @version 1.0
+ * @since 28/08/2020
+ */
+public enum RoleEnum {
+    ROLE_ADMIN, ROLE_USER
+}
