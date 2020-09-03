2ea70d5386dc86f2af6cc5618a2a4420b5d409ba
Angelo Brandão <angelobms@gmail.com>
2020-09-02 21:44:34 -0300

    Added the classes UserConverter and TransactionConverter that converts a data transfer object to an entity and an entity to a data transfer object.

---
+++ src/main/java/br/com/bmsti/currencyconverter/api/converters/TransactionConverter.java
@@ -0,0 +1,49 @@
+package br.com.bmsti.currencyconverter.api.converters;
+
+import br.com.bmsti.currencyconverter.api.dtos.TransactionDTO;
+import br.com.bmsti.currencyconverter.api.entities.Transaction;
+
+/**
+ * Class converter transaction
+ *
+ * @author angelo santos
+ * @version 1.0
+ * @since 31/08/2020
+ */
+public class TransactionConverter {
+
+    /**
+     * Method responsible for converting a transaction to a data transaction object
+     *
+     * @param transactionDTO transaction Data Transfer Object
+     * @return Transaction
+     */
+    public static Transaction covertTo(TransactionDTO transactionDTO) {
+        Transaction transaction = new Transaction();
+        transaction.setId(transactionDTO.getId());
+        transaction.setOriginCurrency(transactionDTO.getOriginCurrency());
+        transaction.setTargetCurrency(transactionDTO.getTargetCurrency());
+        transaction.setOriginValue(transactionDTO.getOriginValue());
+        transaction.setDateTransaction(transactionDTO.getDateTransaction());
+
+        return transaction;
+    }
+
+    /**
+     * Method responsible for converting a data transaction object to a transaction
+     *
+     * @param transaction transaction
+     * @return TransactionDTO
+     */
+    public static TransactionDTO covertTo(Transaction transaction) {
+        TransactionDTO transactionDTO = new TransactionDTO();
+        transactionDTO.setId(transaction.getId());
+        transactionDTO.setUserId(transaction.getUser().getId());
+        transactionDTO.setOriginCurrency(transaction.getOriginCurrency());
+        transactionDTO.setOriginValue(transaction.getOriginValue());
+        transactionDTO.setTargetCurrency(transaction.getTargetCurrency());
+        transactionDTO.setDateTransaction(transaction.getDateTransaction());
+
+        return transactionDTO;
+    }
+}
