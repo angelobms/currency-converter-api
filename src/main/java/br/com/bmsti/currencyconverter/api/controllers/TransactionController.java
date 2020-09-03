3dbd9f591c3f6d76d3aa4fe7eac9ee602ddb59d4
Angelo Brandão <angelobms@gmail.com>
2020-09-02 21:59:38 -0300

    Added class controller transaction.

---
+++ src/main/java/br/com/bmsti/currencyconverter/api/controllers/TransactionController.java
@@ -0,0 +1,64 @@
+package br.com.bmsti.currencyconverter.api.controllers;
+
+import br.com.bmsti.currencyconverter.api.dtos.TransactionDTO;
+import br.com.bmsti.currencyconverter.api.services.TransactionService;
+import br.com.bmsti.currencyconverter.api.services.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Set;
+
+/**
+ * Class controller transaction
+ *
+ * @author angelo santos
+ * @version 1.0
+ * @since 30/08/2020
+ */
+@Slf4j
+@RestController
+@RequestMapping("/api/transactions")
+@CrossOrigin(origins = "*")
+public class TransactionController {
+
+    private final UserService userService;
+
+    private final TransactionService transactionService;
+
+    public TransactionController(UserService userService, TransactionService transactionService) {
+        this.userService = userService;
+        this.transactionService = transactionService;
+    }
+
+    /**
+     * Method responsible for creating a currency conversion transaction
+     *
+     * @param transactionDTO transaction Data Transfer Object
+     * @return ResponseEntity<TransactionDTO> result
+     */
+    @PostMapping
+    public ResponseEntity<TransactionDTO> createTransaction(@Valid @RequestBody TransactionDTO transactionDTO) {
+        log.info("Create transaction: {}", transactionDTO.toString());
+
+        TransactionDTO result = this.transactionService.save(transactionDTO);
+
+        return ResponseEntity.ok(result);
+    }
+
+    /**
+     * Method responsible for searching for currency conversion transactions carried out by a user
+     *
+     * @param userId user id
+     * @return ResponseEntity<Set<TransactionDTO>>
+     */
+    @GetMapping("/{userId}")
+    public ResponseEntity<Set<TransactionDTO>> getTransactionBuUser(@Valid @PathVariable Long userId) {
+
+        Set<TransactionDTO> transactions = this.transactionService.findByUserId(userId);
+        log.info("Create transaction: {}", transactions);
+
+        return ResponseEntity.ok(transactions);
+    }
+}
